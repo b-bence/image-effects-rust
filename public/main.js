2a9e8bdb57d16289700d1255b12cf71dcff4b19e
Bence <borsi92@gmail.com>
2022-03-01 07:56:29 +0100

    Reading files

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log('test');
+function init() {
+
+    const input = document.getElementById('upload')
+    const fileReader = new FileReader()
+
+    fileReader.onloadend = () => {
+        let base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+          );
+    }
+
+    input.addEventListener('change', () => {
+        // Its easier to transfer strings between JS and Rust. FileReader can convert a binary file to a string with base64 encoding
+        fileReader.readAsDataURL(input.files[0])
+    })
+
+}
+
+init()
