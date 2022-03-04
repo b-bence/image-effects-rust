6b9b99f46c96e557b6204f618b7a85d0ff49e89b
Bence <borsi92@gmail.com>
2022-03-04 08:41:36 +0100

    adding base64 decoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
+use base64::decode;
 
 // Macro attribute: wasm bindgen will make the function public to JS. Bindgen crate can export functions
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    // Converts a string to a JS value
-    log(&encoded_file.into());
+    // Converts a string to a JS value with .into()
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
 }
